6133788e4201e77fd3dc8baedfa7f37a54b0d379
vedasree <anapved8@gmail.com>
2025-09-03 10:47:52 +0530

    Example code  covering all SOLID Principles

--- src/main/java/SportsArena.java
+++ src/main/java/SportsArena.java
@@ -2,7 +2,7 @@ public class SportsArena {
     IndoorGames i;
     OutdoorGames o;
     SportsArena(IndoorGames i,OutdoorGames g){
-        this.i=i; this.o=o;
+        this.i=i; this.o=g;
     }
     public void play(){
         System.out.println(i.playIndoor()+" "+o.playOutdoor());
--- src/main/java/SwimmingPool.java
+++ src/main/java/SwimmingPool.java
@@ -1,6 +1,6 @@
 public class SwimmingPool implements OutdoorGames{
     public String playOutdoor(){
-        return "playing outdoor";
+        return "In Swimmingpool";
     }
 
 }
